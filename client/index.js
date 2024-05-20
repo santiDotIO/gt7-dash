fb2ee2e45037b7d484bac4b9f125cd32627f8d8a
Santiago <s.sosa@kickstarter.com>
2024-05-20 01:22:11 -0400

    clean up dash
    Signed-off-by: Santiago <s.sosa@kickstarter.com>

--- client/index.js
+++ client/index.js
@@ -29,7 +29,6 @@ const lightUpShiftPoints = (rpm, revLimit, earlyLights, optimalLights, limitLigh
   }
 }
 
-
 const calculateTyreTemp = (tempNum) => {
   // https://www.yourdatadriven.com/what-should-the-temperature-of-your-racing-car-tyres-be/
   if (tempNum < 60) {
@@ -86,22 +85,22 @@ const totalLapCountFormat = (l) => {
   else if (l === 0) return "\u221E"; // Infinity
   else return l;
 };
+
 // Handle connection open event
 ws.onopen = function(event) {
-  console.log("WebSocket connection opened!"), {event};
+  console.log("WebSocket connection opened!");
+  document.querySelector("#error").innerHTML = '';
 };
 
 // Handle connection error event
 ws.onerror = function(error) {
   console.error("WebSocket connection error:", error);
-  document.querySelector("#error").innerHTML = JSON.stringify(error, null, 2);
-  // alert("WebSocket connection error. Please reload the page.");
+  document.querySelector("#error").innerHTML = `WebSocket connection error: ${JSON.stringify(error, null, 2)}`;
 };
 
 // Handle incoming messages from the server
 ws.onmessage = function(event) {
   const data = JSON.parse(event.data);
-  // console.log(data);
   
   document.querySelector("#rpm").innerText = Math.round(data.engineRPM)
   document.querySelector("#fuel").innerText = Math.round(data.gasLevel)
@@ -109,19 +108,19 @@ ws.onmessage = function(event) {
   // tyre temp
   const fl = document.querySelector("#fl")
   fl.innerText = `${Math.round(data.tireSurfaceTemperature.FrontLeft)}c`
-  fl.style.backgroundColor = calculateTyreTemp(data.tireSurfaceTemperature.FrontLeft)
+  fl.style.setAttribute('data-color', calculateTyreTemp(data.tireSurfaceTemperature.FrontLeft))
   
   const fr = document.querySelector("#fr")
   fr.innerText = `${Math.round(data.tireSurfaceTemperature.FrontRight)}c`
-  fr.style.backgroundColor = calculateTyreTemp(data.tireSurfaceTemperature.FrontRight)
+  fr.style.setAttribute('data-color', calculateTyreTemp(data.tireSurfaceTemperature.FrontRight))
   
   const rl = document.querySelector("#rl")
   rl.innerText = `${Math.round(data.tireSurfaceTemperature.RearLeft)}c`
-  rl.style.backgroundColor = calculateTyreTemp(data.tireSurfaceTemperature.RearLeft)
+  rl.style.setAttribute('data-color', calculateTyreTemp(data.tireSurfaceTemperature.RearLeft))
 
   const rr = document.querySelector("#rr")
   rr.innerText = `${Math.round(data.tireSurfaceTemperature.RearRight)}c`
-  rr.style.backgroundColor = calculateTyreTemp(data.tireSurfaceTemperature.RearRight)
+  rr.style.setAttribute('data-color', calculateTyreTemp(data.tireSurfaceTemperature.RearRight))
   
   document.querySelector(".laps .current").innerText = data.lapCount
   document.querySelector(".laps .total").innerText = totalLapCountFormat(data.lapsInRace)
@@ -131,18 +130,16 @@ ws.onmessage = function(event) {
 
   document.querySelector(".current-gear").innerText = gearFormat(data.currentGear)
   
+  const recommendedGear = document.querySelector(".recommended-gear");
   if (data?.suggestedGear == 15) {
-    document.querySelector(".recommended.down-gear").style.opacity = 0;
-    document.querySelector(".recommended.up-gear").style.opacity = 0;
-  } else if (data.suggestedGear > data.currentGear) {
-    document.querySelector(".recommended.up-gear").innerText = data.suggestedGear
-    document.querySelector(".recommended.up-gear").style.opacity = 1;
-    document.querySelector(".recommended.down-gear").style.opacity = 0;
-  } else if (data.suggestedGear < data.currentGear) {
-    document.querySelector(".recommended.down-gear").innerText = data.suggestedGear
-    document.querySelector(".recommended.up-gear").style.opacity = 0;
-    document.querySelector(".recommended.down-gear").style.opacity = 1;
+    recommendedGear.style.opacity = 0;
+  } else {
+    recommendedGear.innerText = data.suggestedGear
+    recommendedGear.style.opacity = 1;
+    const shiftType = data.suggestedGear > data.currentGear ? "up" : "down";
+    recommendedGear.setAttribute("data-shift", shiftType);
   }
+
   document.querySelector("#brake .pedal-pressure").style.transform = `scaleY(${data.brake/255})`;
   document.querySelector("#throttle .pedal-pressure").style.transform = `scaleY(${data.throttle/255})`;
   
@@ -166,10 +163,6 @@ ws.onmessage = function(event) {
       dot.classList.remove("active");
     }
   });
-  
-  // console.log("Message received from the server:", JSON.parse(event.data));
-  
-  // You can process the received data here
 };
 
 // Handle connection close event
